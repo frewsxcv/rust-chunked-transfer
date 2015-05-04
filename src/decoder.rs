6392f07dde11eefc008ab731a6db50dd586e5e4d
Corey Farwell <coreyf@rwell.org>
2015-05-03 20:50:13 -0400

    Remove all `unwrap`s in read

--- src/decoder.rs
+++ src/decoder.rs
@@ -57,44 +57,24 @@ impl<R> Decoder<R> where R: Read {
 
 impl<R> Read for Decoder<R> where R: Read {
     fn read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
-        // first possibility: we are not in a chunk
-        if self.remaining_chunks_size.is_none() {
-            // trying the read the chunk size
-            let mut chunk_size = Vec::new();
+        let remaining_chunks_size = match self.remaining_chunks_size {
+            Some(c) => c,
+            None => {
+                // first possibility: we are not in a chunk, so we'll attempt to determine
+                // the chunks size
+                let mut chunk_size = Vec::new();
 
-            loop {
-                let byte = match self.source.by_ref().bytes().next() {
-                    Some(b) => try!(b),
-                    None => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
-                };
+                loop {
+                    let byte = match self.source.by_ref().bytes().next() {
+                        Some(b) => try!(b),
+                        None => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
+                    };
 
-                if byte == b'\r' {
-                    break;
-                }
+                    if byte == b'\r' {
+                        break;
+                    }
 
-                chunk_size.push(byte);
-            }
-
-            match self.source.by_ref().bytes().next() {
-                Some(Ok(b'\n')) => (),
-                _ => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
-            }
-
-            let chunk_size = match String::from_utf8(chunk_size) {
-                Ok(c) => c,
-                Err(_) => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError))
-            };
-
-            let chunk_size = match usize::from_str_radix(&chunk_size, 16) {
-                Ok(c) => c,
-                Err(_) => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError))
-            };
-
-            // if the chunk size is 0, we are at EOF
-            if chunk_size == 0 {
-                match self.source.by_ref().bytes().next() {
-                    Some(Ok(b'\r')) => (),
-                    _ => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
+                    chunk_size.push(byte);
                 }
 
                 match self.source.by_ref().bytes().next() {
@@ -102,28 +82,47 @@ impl<R> Read for Decoder<R> where R: Read {
                     _ => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
                 }
 
-                return Ok(0);
+                let chunk_size = match String::from_utf8(chunk_size) {
+                    Ok(c) => c,
+                    Err(_) => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError))
+                };
+
+                let chunk_size = match usize::from_str_radix(&chunk_size, 16) {
+                    Ok(c) => c,
+                    Err(_) => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError))
+                };
+
+                // if the chunk size is 0, we are at EOF
+                if chunk_size == 0 {
+                    match self.source.by_ref().bytes().next() {
+                        Some(Ok(b'\r')) => (),
+                        _ => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
+                    }
+
+                    match self.source.by_ref().bytes().next() {
+                        Some(Ok(b'\n')) => (),
+                        _ => return Err(IoError::new(ErrorKind::InvalidInput, DecoderError)),
+                    }
+
+                    return Ok(0);
+                }
+
+                // now that we now the current chunk size, calling ourselves recursively
+                self.remaining_chunks_size = Some(chunk_size);
+                return self.read(buf);
             }
-
-            // now that we now the current chunk size, calling ourselves recursively
-            self.remaining_chunks_size = Some(chunk_size);
-            return self.read(buf);
-        }
-
-        assert!(self.remaining_chunks_size.is_some());
+        };
 
         // second possibility: we continue reading from a chunk
-        if buf.len() < *self.remaining_chunks_size.as_ref().unwrap() {
+        if buf.len() < remaining_chunks_size {
             let read = try!(self.source.read(buf));
-            *self.remaining_chunks_size.as_mut().unwrap() -= read;
+            self.remaining_chunks_size = Some(remaining_chunks_size - read);
             return Ok(read);
         }
 
         // third possibility: the read request goes further than the current chunk
         // we simply read until the end of the chunk and return
-        assert!(buf.len() >= *self.remaining_chunks_size.as_ref().unwrap());
-
-        let remaining_chunks_size = *self.remaining_chunks_size.as_ref().unwrap();
+        assert!(buf.len() >= remaining_chunks_size);
 
         let buf = &mut buf[.. remaining_chunks_size];
         let read = try!(self.source.read(buf));
